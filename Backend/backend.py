da0cd1ba406de3ed06e99e5eb64b42bd76156a8c
Marco Caudillo <88803666+MarcoCM1101@users.noreply.github.com>
2023-10-26 14:02:14 -0600

    Predict form and file

--- Backend/backend.py
+++ Backend/backend.py
@@ -1,6 +1,9 @@
 from flask import Flask, request, jsonify
 from flask_cors import CORS
 import joblib
+from werkzeug.utils import secure_filename
+import os
+import csv
 
 dt = joblib.load("./static/dt.joblib")
 
@@ -22,9 +25,38 @@ def predict_json():
         float(data["alcohol"])
     ]]
     y_pred = dt.predict(x)
-    print(y_pred)
-
     return jsonify({"result": y_pred[0]})
+
+@app.route("/predict_form", methods=["POST"])
+def predict_form():
+    data = request.form
+    x = [[
+        float(data["pH"]),
+        float(data["sulphates"]),
+        float(data["alcohol"])
+    ]]
+    y_pred = dt.predict(x)
+    return jsonify({"result": y_pred[0]})
+
+@app.route("/predict_file", methods=["POST"])
+def predict_file():
+    file = request.files["archivo"]
+    filename = secure_filename(file.filename)
+    # file.save(f"./static/{filename}")
+    path = os.path.join(os.getcwd(), "static", filename)
+    file.save(path)
+    with open(path, "r") as f:
+        f.readline()
+        reader = csv.reader(f)
+        """ x = []
+        for row in reader:
+            x.append([float(row[0]), float(row[1]), float(row[2])]) """
+        x = [[float(row[0]), float(row[1]), float(row[2])] for row in reader]
+        y_pred = dt.predict(x)
+        return jsonify({
+            "results": [{"key": f"{k}",
+                         "result": f"{v}"} for k, v in enumerate(y_pred)]
+        })
 
 
 if __name__ == "__main__":
